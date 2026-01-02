5ed13164fe6c56e5749ccc8bde42f4b562790b51
ferdoushasanpr <ferdoushasanpr@gmail.com>
2026-01-02 14:54:55 +0600

    feat: GetUserByID Method Added to UserController

--- controllers/userController.js
+++ controllers/userController.js
@@ -13,3 +13,17 @@ exports.getMe = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+exports.getUserById = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: "Invalid User ID format or Server Error" });
+  }
+};
