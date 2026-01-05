8dce25603a293bd7e3df9ec0a3b04b395d473d73
ferdoushasanpr <ferdoushasanpr@gmail.com>
2026-01-06 02:29:10 +0600

    fix: Follow Router Fixed with Auth Middleware

--- routers/followRouter.js
+++ routers/followRouter.js
@@ -1,7 +1,12 @@
 const router = require("express").Router();
 const followController = require("../controllers/followController");
+const authMiddleware = require("../middlewares/authMiddleware");
 
-router.post("/:id/follow", followController.toggleFollow);
-router.get("/:id/is-following", followController.checkFollowStatus);
+router.post("/:id/follow", authMiddleware, followController.toggleFollow);
+router.get(
+  "/:id/is-following",
+  authMiddleware,
+  followController.checkFollowStatus
+);
 
 module.exports = router;
